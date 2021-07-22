665917205156aaa3627da28d7388870389247718
asdsca <a5530960@gmail.com>
2021-07-22 09:34:50 +0800

    Update unlock.sh

--- unlock.sh
+++ unlock.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-    yum install bind-utils
-    yum install -y dnsmasq
-    
-    systemctl enable dnsmasq
-    rm -f /etc/resolv.conf
-    echo "nameserver 127.0.0.1" >> /etc/resolv.conf
-    touch /etc/dnsmasq.d/unlock.conf
-    echo "server=8.8.8.8" >> /etc/dnsmasq.d/unlock.conf
-    echo "server=8.8.4.4" >> /etc/dnsmasq.d/unlock.conf
-    echo "address=/netflix.com/$1" >> /etc/dnsmasq.d/unlock.conf
-    echo "address=/netflix.net/$1" >> /etc/dnsmasq.d/unlock.conf
-    echo "address=/nflximg.net/$1" >> /etc/dnsmasq.d/unlock.conf
-    echo "address=/nflximg.com/$1" >> /etc/dnsmasq.d/unlock.conf
-    echo "address=/nflxvideo.net/$1" >> /etc/dnsmasq.d/unlock.conf
-    echo "address=/nflxso.net/$1" >> /etc/dnsmasq.d/unlock.conf
-    echo "address=/nflxext.com/$1" >> /etc/dnsmasq.d/unlock.conf
-    
-    systemctl restart dnsmasq
+yum install bind-utils
+yum install -y dnsmasq
+systemctl enable dnsmasq
+rm -f /etc/resolv.conf
+echo "nameserver 127.0.0.1" >> /etc/resolv.conf
+touch /etc/dnsmasq.d/unlock.conf
+echo "server=8.8.8.8" >> /etc/dnsmasq.d/unlock.conf
+echo "server=8.8.4.4" >> /etc/dnsmasq.d/unlock.conf
+echo "address=/netflix.com/$1" >> /etc/dnsmasq.d/unlock.conf
+echo "address=/netflix.net/$1" >> /etc/dnsmasq.d/unlock.conf
+echo "address=/nflximg.net/$1" >> /etc/dnsmasq.d/unlock.conf
+echo "address=/nflximg.com/$1" >> /etc/dnsmasq.d/unlock.conf
+echo "address=/nflxvideo.net/$1" >> /etc/dnsmasq.d/unlock.conf
+echo "address=/nflxso.net/$1" >> /etc/dnsmasq.d/unlock.conf
+echo "address=/nflxext.com/$1" >> /etc/dnsmasq.d/unlock.conf
+systemctl restart dnsmasq
