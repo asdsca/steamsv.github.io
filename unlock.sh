b3e2e12ffca116a7f9c3cad92b15d36e897546cb
asdsca <a5530960@gmail.com>
2021-07-22 09:33:15 +0800

    Update unlock.sh

--- unlock.sh
+++ unlock.sh
@@ -4,9 +4,9 @@
     
     systemctl enable dnsmasq
     rm -f /etc/resolv.conf
-    echo "nameserver 127.0.0.1" > /etc/resolv.conf
+    echo "nameserver 127.0.0.1" >> /etc/resolv.conf
     touch /etc/dnsmasq.d/unlock.conf
-    echo "server=8.8.8.8" > /etc/dnsmasq.d/unlock.conf
+    echo "server=8.8.8.8" >> /etc/dnsmasq.d/unlock.conf
     echo "server=8.8.4.4" >> /etc/dnsmasq.d/unlock.conf
     echo "address=/netflix.com/$1" >> /etc/dnsmasq.d/unlock.conf
     echo "address=/netflix.net/$1" >> /etc/dnsmasq.d/unlock.conf
