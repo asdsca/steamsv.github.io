da4a783d9c7ea010d0d041eb8f334bf7fe090eb0
asdsca <a5530960@gmail.com>
2021-07-22 10:26:28 +0800

    Update unlock.sh

--- unlock.sh
+++ unlock.sh
@@ -7,11 +7,18 @@ echo "nameserver 127.0.0.1" >> /etc/resolv.conf
 touch /etc/dnsmasq.d/unlock.conf
 echo "server=8.8.8.8" >> /etc/dnsmasq.d/unlock.conf
 echo "server=8.8.4.4" >> /etc/dnsmasq.d/unlock.conf
-echo "address=/netflix.com/$1" >> /etc/dnsmasq.d/unlock.conf
-echo "address=/netflix.net/$1" >> /etc/dnsmasq.d/unlock.conf
-echo "address=/nflximg.net/$1" >> /etc/dnsmasq.d/unlock.conf
-echo "address=/nflximg.com/$1" >> /etc/dnsmasq.d/unlock.conf
-echo "address=/nflxvideo.net/$1" >> /etc/dnsmasq.d/unlock.conf
-echo "address=/nflxso.net/$1" >> /etc/dnsmasq.d/unlock.conf
-echo "address=/nflxext.com/$1" >> /etc/dnsmasq.d/unlock.conf
+echo "server=/netflix.com/$1" >> /etc/dnsmasq.d/unlock.conf
+echo "server=/netflix.net/$1" >> /etc/dnsmasq.d/unlock.conf
+echo "server=/nflximg.net/$1" >> /etc/dnsmasq.d/unlock.conf
+echo "server=/nflximg.com/$1" >> /etc/dnsmasq.d/unlock.conf
+echo "server=/nflxvideo.net/$1" >> /etc/dnsmasq.d/unlock.conf
+echo "server=/nflxso.net/$1" >> /etc/dnsmasq.d/unlock.conf
+echo "server=/nflxext.com/$1" >> /etc/dnsmasq.d/unlock.conf
+echo "server=/bilibili.com/$1" >> /etc/dnsmasq.d/unlock.conf
+echo "server=/app.bilibili.com/$1" >> /etc/dnsmasq.d/unlock.conf
+echo "server=/api.bilibili.com/$1" >> /etc/dnsmasq.d/unlock.conf
+echo "server=/data.bilibili.com/$1" >> /etc/dnsmasq.d/unlock.conf
+echo "server=/interface.bilibili.com/$1" >> /etc/dnsmasq.d/unlock.conf
+echo "server=/grpc.biliapi.net/$1" >> /etc/dnsmasq.d/unlock.conf
+echo "server=/dataflow.biliapi.com/$1" >> /etc/dnsmasq.d/unlock.conf
 systemctl restart dnsmasq
